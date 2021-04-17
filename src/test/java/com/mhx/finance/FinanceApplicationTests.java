959c1001b015320deabf24658f6900f4ea9d1f64
DreamsMu <744535595@qq.com>
2021-04-17 19:35:13 +0800

    add file

---
+++ src/test/java/com/mhx/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhx.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
